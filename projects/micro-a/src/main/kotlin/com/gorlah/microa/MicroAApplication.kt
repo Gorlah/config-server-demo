ef85d6eab51e79852c5cde9de7e81f9f7e0dd3e5
Eric <eah0592@gmail.com>
2021-01-14 22:15:45 -0600

    Enable config server

--- projects/micro-a/src/main/kotlin/com/gorlah/microa/MicroAApplication.kt
+++ projects/micro-a/src/main/kotlin/com/gorlah/microa/MicroAApplication.kt
@@ -1,11 +1,25 @@
 package com.gorlah.microa
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class MicroAApplication
 
 fun main(args: Array<String>) {
 	runApplication<MicroAApplication>(*args)
+}
+
+@Component
+class Test(
+	@Value("\${property}") private val property: String?,
+) {
+
+	@PostConstruct
+	fun postConstruct() {
+		println(property)
+	}
 }
--- projects/micro-b/build.gradle.kts
+++ projects/micro-b/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
