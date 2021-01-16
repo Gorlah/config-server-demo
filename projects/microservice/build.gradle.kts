5aadb460bd7c8186ec5b645a933262c6b265e409
Eric <eah0592@gmail.com>
2021-01-16 10:26:04 -0600

    Remove spring-cloud-starter-bootstrap dependency in favor of "spring.cloud.import" property

--- projects/microservice/build.gradle.kts
+++ projects/microservice/build.gradle.kts
@@ -12,7 +12,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
