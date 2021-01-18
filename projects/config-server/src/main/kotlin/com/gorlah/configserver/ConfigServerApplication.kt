bc68cf3f4725d00e7ba6691788997c77ef7b5771
Gorlah <eah0592@gmail.com>
2021-01-18 08:48:45 -0600

    Remove unnecessary logger function from Config Server

--- projects/config-server/src/main/kotlin/com/gorlah/configserver/ConfigServerApplication.kt
+++ projects/config-server/src/main/kotlin/com/gorlah/configserver/ConfigServerApplication.kt
@@ -1,10 +1,8 @@
 package com.gorlah.configserver
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
-import kotlin.reflect.KClass
 
 @EnableConfigServer
 @SpringBootApplication
@@ -13,5 +11,3 @@ class ConfigServerApplication
 fun main(args: Array<String>) {
     runApplication<ConfigServerApplication>(*args)
 }
-
-fun <T : Any> logger(kClass: KClass<T>) = LoggerFactory.getLogger(kClass::class.java)!!
