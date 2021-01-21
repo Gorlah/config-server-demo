b9895322df40794c178f7d5490ddde443095d6cb
Gorlah <eah0592@gmail.com>
2021-01-20 19:48:54 -0600

    Clean up project files

--- projects/user-server/src/main/kotlin/com/gorlah/user/server/property/PropertyController.kt
+++ projects/user-server/src/main/kotlin/com/gorlah/user/server/property/PropertyController.kt
@@ -1,14 +1,19 @@
 package com.gorlah.user.server.property
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RefreshScope
 @RestController
-class PropertyController(@Value("\${myproperty}") private val myproperty: String) {
+@RequestMapping("/properties")
+class PropertyController(
+    private val environment: Environment,
+) {
 
-    @GetMapping("/myproperty")
-    fun myproperty() = myproperty
+    @GetMapping("/{property}")
+    fun getProperty(@PathVariable property: String) = environment.getProperty(property)
 }
--- projects/user-server/src/main/kotlin/com/gorlah/user/server/swagger/SwaggerConfig.kt
+++ projects/user-server/src/main/kotlin/com/gorlah/user/server/swagger/SwaggerConfig.kt
@@ -2,8 +2,10 @@ package com.gorlah.user.server.swagger
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
+import java.util.function.Predicate
 
 @Configuration
 class SwaggerConfig {
@@ -11,6 +13,6 @@ class SwaggerConfig {
     @Bean
     fun swagger() = Docket(DocumentationType.OAS_30)
         .select()
-        .paths { !it.matches(Regex("/error")) }
+        .paths(Predicate.not(PathSelectors.ant("/error/**")))
         .build()!!
 }
